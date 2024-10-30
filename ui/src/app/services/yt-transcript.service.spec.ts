bce714e05cc227ee095608c80bf7756129989aaa
Lucca Jiménez Könings <development@jimkoen.com>
2024-10-30 23:00:40 +0100

    remove spinner activation in preparation for moving video and transcript resolution into their own resolvers
    Signed-off-by: Lucca Jiménez Könings <development@jimkoen.com>

---
+++ ui/src/app/services/yt-transcript.service.spec.ts
@@ -0,0 +1,60 @@
+import { TestBed } from '@angular/core/testing';
+
+import {YouTubeTranscriptTimestamp, YtTranscriptService} from './yt-transcript.service';
+import {HttpTestingController, provideHttpClientTesting} from "@angular/common/http/testing";
+import {HttpErrorResponse, provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
+import {TRANSCRIPTS} from "../../../../api/tests/fixtures/frontend/TRANSCRIPT";
+
+describe('YtTranscriptService', () => {
+  let yt : YtTranscriptService,
+    httpTestingController : HttpTestingController
+
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [
+        YtTranscriptService,
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting()
+      ]
+    });
+    yt = TestBed.inject(YtTranscriptService);
+    httpTestingController = TestBed.inject(HttpTestingController)
+  });
+
+
+
+  fit('should be created', () => {
+    expect(yt).toBeTruthy();
+  });
+
+  fit('should retrieve transcript', () => {
+    yt.getTranscript('bZe5J8SVCYQ')
+      .subscribe((transcript) =>
+        transcript.forEach((ts, i) => {
+          expect(ts).toEqual(TRANSCRIPTS.bZe5J8SVCYQ[i])
+        })
+      )
+
+    const req = httpTestingController.expectOne('api/transcripts/bZe5J8SVCYQ')
+
+    expect(req.request.method).toEqual("GET")
+    req.flush(TRANSCRIPTS.bZe5J8SVCYQ)
+  })
+
+  fit('should give a 503 when transcript is disabled or video not found', () => {
+    yt.getTranscript('')
+      .subscribe({
+        next: () => fail('transcript retrieval should have failed'),
+        error: (e : HttpErrorResponse) => {
+          expect(e.status === 503)
+          expect(e.message === "Error while retrieving transcripts: either disabled or not found")
+        }
+  }
+      )
+    const req = httpTestingController.expectOne('api/transcripts/')
+    expect(req.request.method).toEqual("GET")
+    req.flush("Transcript retrieval failed", {status: 503, statusText: "Error while retrieving transcripts: either disabled or not found"})
+  })
+});
---
+++ ui/src/app/services/yt-transcript.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export type YouTubeTranscriptTimestamp = {
+        text: string,
+        start: number,
+        duration: number
+}
+
+
+// taken from https://github.com/angular/components/blob/main/src/youtube-player/youtube-player.ts#L97
+
+
+enum PlayerState {
+        UNSTARTED = -1,
+        ENDED = 0,
+        PLAYING = 1,
+        PAUSED = 2,
+        BUFFERING = 3,
+        CUED = 5
+}
+
+
+
+@Injectable({
+        providedIn: 'root'
+})
+export class YtTranscriptService {
+        private apiUrl = "api"
+
+
+        constructor(private httpClient: HttpClient) { }
+
+        getTranscript(videoId: string): Observable<YouTubeTranscriptTimestamp[]> {
+                const url = `${this.apiUrl}/transcripts/${videoId}`
+                return this.httpClient.get<YouTubeTranscriptTimestamp[]>(url)
+        }
+
+
+
+
+
+}
